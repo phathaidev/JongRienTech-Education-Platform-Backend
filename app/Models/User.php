17d54f106a8b03f778665320b3df5daa9065b06e
phathai.yan <phathai.yan@gmail.com>
2026-09-21 14:56:52 +0700

    Add migration for users table and seeder for an admin user

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'verified',
+        'profile_picture',
+        'remember_token'
     ];
 
     /**
