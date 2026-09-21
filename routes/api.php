5c839fab01e670fbdb77e80209ca780243a09519
phathai.yan <phathai.yan@gmail.com>
2026-09-21 22:43:40 +0700

    Add api route

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\V1\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])
+        ->middleware('throttle:5, 1'); // rate limiting, 5 attempts per minute
+});
