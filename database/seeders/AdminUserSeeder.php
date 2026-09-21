a143a9634fcdd86ab5ad487a943598ba6c332169
phathai.yan <phathai.yan@gmail.com>
2026-09-21 22:42:45 +0700

    Adjust AdminUserSeeder by checking for existing email and password from .env file first

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -15,6 +14,16 @@ class AdminUserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Check for existing ADMIN_EMAIL and ADMIN_PASSWORD from .env first
+        $adminEmail = env('ADMIN_EMAIL');
+        $adminPassword = env('ADMIN_PASSWORD');
+        if (blank($adminEmail)) {
+            throw new \RuntimeException('Admin Email is not being set in .env file');
+        }
+        if (blank($adminPassword)) {
+            throw new \RuntimeException('Admin Password is not being set in .env file');
+        }
+
         User::updateOrCreate(
             ['email' => env('ADMIN_EMAIL', 'admin@example.com')],
             [
